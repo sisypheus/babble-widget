4d17b1fe57b34596a12de6b07c74cf9dfdef6fbb
Theo <theo.poette@epitech.eu>
2022-01-01 18:35:38 +0100

    feat: basic test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import icon from "./assets/chat_icon.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="fixed bottom-0 right-0">
+      Test
+      <img src={icon} className="w-16 h-16" alt="chat icon" />
     </div>
   );
 }

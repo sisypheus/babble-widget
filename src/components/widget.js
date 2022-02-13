3085b586e240adfe97e6e72ee86f594bc27035f2
Theo <theo.poette@epitech.eu>
2022-02-13 15:21:40 +0100

    feat: animation on opening the chat window

--- src/components/widget.js
+++ src/components/widget.js
@@ -13,11 +13,13 @@ const Widget = () => {
 
 	return (
 		<div className='reset'>
-			<div className='fixed bottom-0 right-0 p-6 mb-[6rem]'>
-				{widgetOpen && (
-					<div className='bg-gray-200 h-[30rem] w-[18rem] rounded-md'>
+			<div className='fixed bottom-0 right-0 sm:p-6 sm:mb-[4rem]'>
+				<div onClick={() => (console.log('click'))} className={`transition-all w-screen h-screen duration-200 ease-in bg-gray-200 sm:h-[30rem] sm:w-[18rem] rounded-md ${widgetOpen ? 'opacity-100 sm:-translate-y-10' : 'transition-none absolute invisible opacity-0 '}`}>
+					<div>top</div>
+					<div className='relative'>
+						<div className='fixed bottom-0'>bottom</div>
 					</div>
-				)}
+				</div>
 				<div className='fixed bottom-0 right-0 p-6'>
 					<img onClick={handleClick} className='bg-blue-600 p-4 rounded-md w-8 h-8 cursor-pointer' src={chat_icon} alt='chat' />
 				</div>
